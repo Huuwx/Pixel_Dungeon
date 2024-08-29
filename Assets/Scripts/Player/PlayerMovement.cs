1ffbb021ab3c7e169be8801f2a6f87d100a35938
Huuwx <chumanhhuu2@gmail.com>
2024-08-29 18:40:59 +0700

    some place cant move through

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -8,12 +9,16 @@ public class PlayerMovement : MonoBehaviour
 
     public Vector3 input = new Vector3();
 
+    public Vector2 targetPos;
+
     public float moveSpeed = 1f;
 
     public bool isRunning = false;
     private bool canRun = true;
 
     private Animator animator;
+
+    public LayerMask HiddenWall;
 
     // Start is called before the first frame update
     void Start()
@@ -29,26 +34,28 @@ public class PlayerMovement : MonoBehaviour
             input = InputManager.Instance.InputFromKeyBoard(input);
             if (input != Vector3.zero)
             {
-                //var targetPos = transform.position;
-                //targetPos.x += input.x;
-                //targetPos.y += input.y;
-
                 var currentPos = transform.position;
 
                 input.Normalize();
                 facingDir = input;
-                Movement(currentPos, input.x, input.y);
                 if (input.x > 0)
                 {
                     transform.localScale = Vector3.one;
-
                 }
                 else if (input.x < 0)
                 {
                     transform.localScale = new Vector3(-1, 1, 1);
                 }
-                isRunning = true;
-                animator.SetBool("IsRunning", isRunning);
+
+                targetPos.x = input.x;
+                targetPos.y = input.y;
+
+                if (isWalkable(targetPos))
+                {
+                    Movement(currentPos, input.x, input.y);
+                    isRunning = true;
+                    animator.SetBool("IsRunning", isRunning);
+                }
             }
             else
             {
@@ -74,5 +81,30 @@ public class PlayerMovement : MonoBehaviour
     public void setFalseCanRun()
     {
         canRun = false;
+    }
+
+    public bool isWalkable(Vector3 targetPos)
+    {
+        //RaycastHit2D[] ray = Physics2D.RaycastAll(transform.position, targetPos.normalized * 0.7f, 0.7f);
+
+        //foreach(RaycastHit2D rayHit in ray)
+        //{
+        //    if(rayHit.collider.tag == "Wall")
+        //    {
+        //        return false;
+        //    }
+        //}
+        //return true;
+
+        if(Physics2D.Raycast(transform.position, targetPos.normalized * 0.7f, 0.7f, HiddenWall))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawRay(transform.position, targetPos.normalized * 0.7f);
     }
 }
